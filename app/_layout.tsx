f1a584b732db5cbc05a76f3d63bc9c8b6d0e561c
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2026-01-11 21:44:49 +0200

    REFACTOR: moving the current weather to its own component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,66 +1,21 @@
 import React from "react";
-import {Text, View, StyleSheet} from "react-native"
-import {SafeAreaView} from "react-native-safe-area-context";
-import {Feather} from "@expo/vector-icons";
+import {View, StyleSheet} from "react-native";
+import {CurrentWeather} from "@/components/CurrentWeather";
 
 export default function RootLayout() {
     return (
-        <SafeAreaView style={styles.wrapper}>
-            <View style={styles.container}>
-                <Feather name="sun" size={100} color="black" />
-                <Text>Current Weather</Text>
-                <Text style={styles.temp}>6</Text>
-                <Text style={styles.feels}>Feels like 5</Text>
-                <View style={styles.highLowWrapper}>
-                    <Text style={styles.highLow}>High: 8 </Text>
-                    <Text style={styles.highLow}>Low: 6</Text>
-                </View>
-            </View>
-            <View style={styles.bodyWrapper}>
-                <Text style={styles.description}>Its sunny</Text>
-                <Text style={styles.message}>Its perfect t-shirt weather</Text>
-            </View>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <CurrentWeather />
+        </View>
     )
 }
+
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: "center",
-        flex: 1,
-        alignItems: "center"
-    },
-    wrapper: {
-        flex: 1,
-        backgroundColor: 'pink',
-    },
-    temp: {
-        color: 'black',
-        fontSize: 48
-    },
-    feels: {
-        fontSize: 30,
-        color: 'black'
-    },
-    highLow: {
-        color: 'black',
-        fontSize: 20
-    },
-    highLowWrapper: {
-        flexDirection: 'row'
-    },
-    bodyWrapper: {
-        justifyContent: 'flex-end',
-        alignItems: 'flex-start',
-        paddingLeft: 25,
-        marginBottom: 40
-    },
-    description: {
-        fontSize: 48
-    },
-    message: {
-        fontSize: 30
-    }
+container : {
+    flex: 1
+}
 })
+
 // import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 // import { Stack } from 'expo-router';
 // import { StatusBar } from 'expo-status-bar';
