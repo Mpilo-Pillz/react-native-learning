b32b977e46d854044050e1d113d282db5b2909a8
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2026-01-13 19:09:24 +0200

    REFACTOR: rendering upcomign weather

--- components/UpcomingWeather.tsx
+++ components/UpcomingWeather.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {SafeAreaView} from "react-native-safe-area-context";
-import {StyleSheet, Text, FlatList} from 'react-native';
+import {StyleSheet, Text, FlatList, View} from 'react-native';
+import {Feather} from "@expo/vector-icons";
+
 
 const DATA = [
     {
@@ -41,11 +43,26 @@ const DATA = [
     }
 ];
 
+const Item = (props) => {
+    const {dt_text, min, max, condition} = props
+    return (
+<View>
+    <Feather name={'sun'} size={50} color={'white'}/>
+    <Text>{dt_text}</Text>
+    <Text>{min}</Text>
+    <Text>{max}</Text>
+</View>
+    )
+}
+
 export const UpcomingWeather = () => {
+    const renderItem = ({item}) => (
+        <Item condition={item.weather[0].main} dt_text={item.dt_txt} min={item.main.temp_min} max={item.main.temp_max} />
+    )
     return (
         <SafeAreaView style={styles.container} >
 <Text>Upcoming Weather</Text>
-            <FlatList data={DATA} renderItem={} />
+            <FlatList data={DATA} renderItem={renderItem} />
         </SafeAreaView>
     )
 }
