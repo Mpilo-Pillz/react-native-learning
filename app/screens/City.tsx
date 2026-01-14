f09cb2edcb246743cd94a22279cecd843d60d75e
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2026-01-14 09:00:47 +0200

    WIP: styling the city screen

--- app/screens/City.tsx
+++ app/screens/City.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, ImageBackground, StatusBar} from 'react-native';
 import {SafeAreaView} from "react-native-safe-area-context";
 
 export const City = () => {
-    const {container} = styles
+    const {container, imageLayout, cityName, countryName, placeText} = styles
     return (
         <SafeAreaView style={container}>
-            <Text>this is a city</Text>
+            <ImageBackground style={imageLayout} source={require('../../assets/city.jpg')}>
+                <Text style={[cityName, placeText]}>Jhb</Text>
+                <Text style={[countryName, placeText]}>Mzansi</Text>
+            </ImageBackground>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0
+    },
+    imageLayout: {
         flex: 1
+    },
+    cityName: {
+        fontSize: 40,
+    },
+    countryName: {
+        fontSize: 30,
+    },
+    placeText: {
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white'
     }
 })
