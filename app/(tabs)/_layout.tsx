d920306e511704ffc2177216b827d5ff3a6fc165
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2026-01-19 07:41:11 +0200

    adding active state to tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Feather } from "@expo/vector-icons";
 
 
 export default function TabLayout() {
@@ -13,13 +14,24 @@ export default function TabLayout() {
     return (
         <Tabs
             screenOptions={{
-                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'grey',
                 headerShown: false,
                 tabBarButton: HapticTab,
             }}>
-            <Tabs.Screen name="current" options={{ title: "Current" }} />
-            <Tabs.Screen name="upcoming" options={{ title: "Upcoming" }} />
-            <Tabs.Screen name="city" options={{ title: "City" }} />
+            <Tabs.Screen name="current" options={{ title: "Current", tabBarIcon: ({focused}) => (
+                <Feather name={'droplet'} size={25} color={focused ? 'tomato' : 'black'}/>
+                ) }} />
+            <Tabs.Screen
+                name="upcoming"
+                options={{ title: "Upcoming", tabBarIcon: ({focused}) => (
+                        <Feather name={'clock'} size={25} color={focused ? 'tomato' : 'black'}/>
+                    ) }}
+
+            />
+            <Tabs.Screen name="city" options={{ title: "City", tabBarIcon: ({focused}) => (
+                    <Feather name={'home'} size={25} color={focused ? 'tomato' : 'black'}/>
+                ) }} />
         </Tabs>
     );
 }
